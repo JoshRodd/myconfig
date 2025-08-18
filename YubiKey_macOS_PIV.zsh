a79928934a40ced3ae88a6ec142d91d1c2ac536b
Josh Rodd <josh@rodd.us>
2025-08-17 22:09:21 -0400

    Improvements

--- YubiKey_macOS_PIV.zsh
+++ YubiKey_macOS_PIV.zsh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 # YubiKey_macOS_piv.sh v0.1.0
+# Licensed under MIT license; see the file LICENSE for details.
 
 function defaults {
 	default_mgmt_key=010203040506070801020304050607080102030405060708
@@ -13,13 +14,14 @@ function defaults {
 
 function process_options {
 	arg_options=(--mgmt-key --user-pin --spin-hz --prompt-timeout)
-	bool_options=(--dry-run --quiet)
-	while [[ $# -gt 0 ]]; then
+	bool_options=(--dry-run --quiet --force --wipe)
+	while [ $# -gt 0 ]; do
 		if [[ $1 == --version ]]; then
 			echo YubiKey_macOS_piv.sh v0.1.0
 			exit 0
 		elif [[ $1 == --help ]]; then
 			cat <<EOD
+
 Sets up a YubiKey as a PIV-enabled smartcard on macOS for non-password system
 login.
 
@@ -34,19 +36,25 @@ Options:
 	--prompt-timeout TIMEOUT_SECONDS:
 			Changes the timeout for the user to remove and reinsert
 			their YubiKey. The default is $default_timeout seconds.
-	--dry-run, -n:	Prints the commands that would be run instead of
+	--dry-run:	Prints the commands that would be run instead of
 			actually running them.
+	--wipe:		Replaces the current pairing even if it already seems
+			exist and be valid.
 
 Requires $required_macos_version_descr or later unless the --dry-run option is used.
 
-EOD;	exit 0
+EOD
+			exit 0
 		elif [[ $1 == --no-* ]]; then
 			opt=$1
 			shift
-		elif [[ ${bool_options[(ie)$1]} -le ${#bool_otions} ]]; then
+		elif [[ ${bool_options[(ie)$1]} -le ${#bool_options} ]]; then
 			opt=$1
+			value=$1
 			shift
-			declare "$opt=$opt"
+			opt=${opt/--/}
+			opt=${opt//-/_}
+			eval "$opt=$value"
 		elif [[ $1 == --default-* ]]; then
 			opt=$1
 			shift
@@ -61,42 +69,42 @@ EOD;	exit 0
 			opt=$1
 			value=$2
 			shift 2
-			opt=${1/--/}
+			opt=${opt/--/}
 			opt=${opt//-/_}
-			declare "$opt=$value"
-			shift
+			eval "$opt=$value"
 		else
 			printf "%s: invalid option '%q'\n" "$0" "$1"
 			exit 1
 		fi
-	fi
+	done
 	if [[ -z $mgmt_key ]]; then mgmt_key=$default_mgmt_key; fi
 	if [[ -z $user_pin ]]; then user_pin=$default_user_pin; fi
-	if [[ -z $spin_hz ]]; then spin_hz=$spin_hz; fi
-	if [[ -z $prompt_timeout ]]; then prompt_timeout=$prompt_timeout; fi
-	prompt_timeout_sec=$[$prompt_timeout * $spin_hz]
+	if [[ -z $spin_hz ]]; then spin_hz=$default_spin_hz; fi
+	if [[ -z $prompt_timeout ]]; then prompt_timeout=$default_prompt_timeout; fi
+	prompt_timeout_sec="$[$prompt_timeout * $spin_hz]"
 }
 
-process_options
-[[ -z $quiet && -z $dry_run ]] && printf "Enrolling your YubiKey.\n" >&2
-if [[ -z $dry_run ]]; then
-	if [[ $(uname) != Darwin ]]; then
-		printf "This script can only be run on macOS. You can use --dry-run\n" >&2
-		printf "or -n option see the commands that would be run.\n" >&2
-		exit 1
+function check_os {
+	if [[ -z $dry_run ]]; then
+		if [[ $(uname) != Darwin ]]; then
+			printf "This script can only be run on macOS. You can use --dry-run\n" >&2
+			printf "or -n option see the commands that would be run.\n" >&2
+			exit 1
+		fi
+		current_macos_version=$(uname -r)
+		sorted=$(echo -e "$current_macos_version\n$required_macos_version" | sort -V)
+		if [[ $(echo "$sorted" | head -n 1) != $required_macos_version && $current_macos_version != $required_macos_version ]]; then
+			printf "This script requires %s or later.\n" "$required_macos_version_descr" >&2
+			exit 1
+		fi
 	fi
-	current_macos_version=$(uname -r)
-	sorted=$(echo -e "$current_macos_version\n$required_macos_version" | sort -V)
-	if [[ $(echo "$sorted" | head -n 1) != $required_macos_version && $current_macos_version != $required_macos_version ]]; then
-		printf "This script requires %s or later.\n" "$required_macos_version_descr" >&2
-		exit 1
-	fi
-fi
+}
 
 function run_cmd {
+	cmd=$1
+	shift
 	if [[ -z $dry_run ]]; then
-		if [[ ! -x $(command -v $1) ]]
-			cmd=$1
+		if [[ ! -x $(command -v $cmd) ]]; then
 			printf "The command '%s' isn't available.\n" "$cmd" >&2
 			if [[ $cmd == "ykman" ]]; then
 				printf "\nTry installing it via Homebrew with:\n\n" >&2
@@ -105,15 +113,19 @@ function run_cmd {
 				printf "Ensure it is in your PATH.\n" >&2
 			fi
 			exit 1
+		else
+			$cmd "$@"
 		fi
 	else
+		idx=0
 		for arg in "$@"; do
+			idx=$[$idx + 1]
 			if [[ $arg == *[\'\"]* ]]; then
 				printf "'%q'" $arg
 			else
 				printf "%q" $arg
 			fi
-			if [[ $(( $# - ${#argv[@]} )) -eq $(($# - 1)) ]]; then
+			if [[ $# -eq $idx ]]; then
 				printf "\n"
 			else
 				printf " "
@@ -121,6 +133,24 @@ function run_cmd {
 		done
 	fi
 }
+
+defaults
+process_options "$@"
+check_os
+
+function get_active {
+	active_hash=$(sc_auth identities awk -v user="$USER" '/Paired identities which are used for authentication:/{p=1;next} p&&/Unpaired identities:/{p=010} &&$2==user{print $1}')
+}
+
+# Ensure the active YubiKey isn't already configured.
+if [[ -z $wipe ]]; then
+	get_active
+	if [[ $active_hash != "" ]]; then
+		printf "%s: Your YubiKey appears to already be configured. To replace\n" $0 >&2
+		printf "it anyway, run:\n\n\t%s --wipe\n" $0 >&2
+		exit 0
+	fi
+fi
 
 run_cmd ykman piv reset --force || exit
 for topic in 9a 9d; do
